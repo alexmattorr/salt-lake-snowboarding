2c26a7baffb42734a01d2c349cc70887ea2b8428
Alexander Orr <alexanderorr@Alexanders-MacBook-Pro.local>
2016-11-26 17:20:41 -0700

    basic stuff

--- wp-content/themes/sls/src/index.php
+++ wp-content/themes/sls/src/index.php
@@ -12,11 +12,14 @@
       $image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 ?>
 
-      <li><a href="<?php the_permalink(); ?>">
+      <li class="post"><a href="<?php the_permalink(); ?>">
         <figure style="background-image: url(<?php echo $image; ?>);"></figure>
-        <h4><?php the_title(); ?></h4>
-        <h6><?php the_time('F j, Y'); ?></h6>
-        <p><?php the_excerpt(); ?></p>
+
+        <div class="post-info">
+          <h4><?php the_title(); ?></h4>
+          <h6><?php the_time('F j, Y'); ?></h6>
+          <p><?php the_excerpt(); ?></p>
+        </div>
       </a></li>
 
 <?php
